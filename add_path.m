05b15102ae34c7a0bcf728a8cc870c6aff78f787
sherwood <zhengxiawu@126.com>
2016-06-01 21:05:50 +0800

    add_files

---
+++ add_path.m
@@ -0,0 +1,7 @@
+%add path
+matconv_root = '/home/sherwood/project/matlab/matconvnet-1.0-beta19';
+addpath(fullfile(matconv_root,'matlab'));
+addpath(fullfile(matconv_root,'matlab','mex'));
+addpath(fullfile(matconv_root,'matlab','xtest'));
+addpath(fullfile(matconv_root,'matlab','simplenn'));
+addpath('tools/');
