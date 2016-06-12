87c9ed49f3f10a1dae8d8a9087445fd63a8984ed
sherwood <zhengxiawu@126.com>
2016-06-12 20:22:11 +0800

    add a judge that if use sum matrix

--- tools/holiday_script/holiday_retrieval.m
+++ tools/holiday_script/holiday_retrieval.m
@@ -2,11 +2,20 @@ add_path();
 rep_range = [15 17];
 input_rep = {};
 rep = [];
+%caculate_method ==1 means sum 
+caculate_method = 0;
 for i=rep_range
     pre_rep = importdata(['feature/holiday/imagenet-matconvnet-alex_',int2str(i),'.mat']);
-    rep = [pre_rep rep];   
+    if caculate_method ==0
+        input_rep{end+1} = pre_rep';
+    else
+        rep = [pre_rep rep];
+        if i==rep_range(end)
+           input_rep{end+1} = rep'; 
+        end
+    end
 end
-input_rep{end+1} = rep';
+
 query = importdata('feature/holiday/query.mat');
 PCA_range = [32 64 128 256 512];
 [~,map] = retrieval_pipeline(query,input_rep,PCA_range);
